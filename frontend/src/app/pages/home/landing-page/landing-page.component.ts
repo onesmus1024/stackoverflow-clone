077fe418d3f85ef88c8e4c3ef2aee164bdb2289f
Onesmus Wambugu <onesmus1024@gmail.com>
2023-03-12 15:10:54 +0300

    update:added platform section

--- frontend/src/app/pages/home/landing-page/landing-page.component.ts
+++ frontend/src/app/pages/home/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, MatIconModule],
+  imports: [CommonModule, MatIconModule, RouterModule],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
