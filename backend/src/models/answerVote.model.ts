fdef18e22472a38e2336e8a87fb11aa8dc5747d1
Onesmus Wambugu <onesmus1024@gmail.com>
2023-03-15 09:53:53 +0300

    update:modified answer vote model to include votes

--- backend/src/models/answerVote.model.ts
+++ backend/src/models/answerVote.model.ts
@@ -1,23 +1,23 @@
 
-class answerVoteModel {
+class AnswerVoteModel {
     id: string;
-    comment: string;
     created_at: string;
     updated_at: string;
     user_id: string;
     answer_id: string;
     is_deleted: string;
+    vote: string;
 
-    constructor(id: string, comment: string, created_at: string, updated_at: string, user_id: string, answer_id: string, is_deleted: string) {
+    constructor(id: string, created_at: string, updated_at: string, user_id: string, answer_id: string, is_deleted: string, vote: string) {
         this.id = id;
-        this.comment = comment;
         this.created_at = created_at;
         this.updated_at = updated_at;
         this.user_id = user_id;
         this.answer_id = answer_id;
         this.is_deleted = is_deleted;
+        this.vote = vote;
     }
 }
 
 
-export default answerVoteModel
+export default AnswerVoteModel
