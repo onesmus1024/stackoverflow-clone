87047139513771a2deb97d08c0c25c52ee9f73ba
Onesmus Wambugu <onesmus1024@gmail.com>
2023-03-19 20:45:14 +0300

    update : paginated questions

--- backend/database/storedProcedure/question/getAllQuestions.sp.sql
+++ backend/database/storedProcedure/question/getAllQuestions.sp.sql
@@ -1,10 +1,17 @@
 
+-- pagination for questions table 
 
 USE STACKOVERFLOW;
 GO
 
 CREATE OR ALTER PROCEDURE getAllQuestions
+    @page INT,
+    @pageSize INT
 AS
 BEGIN
     SELECT * FROM questions
+    ORDER BY id
+    OFFSET (@page - 1) * @pageSize ROWS
+    FETCH NEXT @pageSize ROWS ONLY
 END
+
